c7e378f211d8ab4ee8bd140b2dbc7ac3f5a6e87e
Adam Plumb <adamplumb@gmail.com>
2009-12-16 13:21:06 -0500

    Updated some copyright info

--- src/thunarx-python.c
+++ src/thunarx-python.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2006 Peter de Ridder <peter@xfce.org>
+ * Copyright (c) 2009 Adam Plumb <adamplumb@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
