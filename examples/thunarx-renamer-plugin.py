35310096812bbe7b7e21d25c61cb8791c15b9c39
Adam Plumb <adamplumb@gmail.com>
2010-01-13 11:04:31 -0500

    Redefined some ThunarxRenamer methods as virtual and set up override functions for the ThunarxRenamer.get_actions virtual method.  Also updated the renamer plugin example

--- examples/thunarx-renamer-plugin.py
+++ examples/thunarx-renamer-plugin.py
@@ -16,7 +16,7 @@ class ThunarxPythonRenamer(thunarx.Renamer):
         hbox.pack_start(label, False, False, 0)
         
         entry = gtk.Entry()
-        entry.connect("changed", self.changed)
+        entry.connect("changed", self.do_changed)
         hbox.pack_start(entry, False, False, 0)
         
         label.show()
@@ -24,14 +24,14 @@ class ThunarxPythonRenamer(thunarx.Renamer):
         self.add(hbox)
         hbox.show()        
     
-    def process(self, file, text, index):
+    def do_process(self, file, text, index):
         return "__" + text
 
-    def changed(self, widget):
-        print "changed"
+    def do_changed(self, widget):
+        pass
 
-    do_process = process
-    do_changed = changed
+    def do_get_actions(self, window, files):
+        return [gtk.Action("TPR:SomeAction", "Some Action", None, gtk.STOCK_OPEN)]
 
 class ThunarxRenamerPlugin(thunarx.RenamerProvider):
     def __init__(self):
