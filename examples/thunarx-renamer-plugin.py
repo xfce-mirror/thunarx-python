adee09c9457f447e7a5ab73babf336c51901401f
Adam Plumb <adamplumb@gmail.com>
2010-01-13 16:22:22 -0500

    Implemented the do_save and do_load ThunarxRenamer proxy methods and cleaned things up a bit

--- examples/thunarx-renamer-plugin.py
+++ examples/thunarx-renamer-plugin.py
@@ -1,15 +1,20 @@
 import thunarx
 import gtk
+import gobject
 
 class ThunarxPythonRenamer(thunarx.Renamer):
     __gtype_name__ = "ThunarxPythonRenamer"
+    test = gobject.property(type=str)
     
     def __init__(self):
         thunarx.Renamer.__init__(self)
+
+        # Set properties to be saved in the settings files
+        self.set_property("test", "blah")
         
         self.set_name("Adam's Renamer")
         self.set_help_url("http://www.google.com")
-        
+     
         hbox = gtk.HBox(0, False)
         
         label = gtk.Label("Setting 1:")
@@ -22,16 +27,34 @@ class ThunarxPythonRenamer(thunarx.Renamer):
         label.show()
         entry.show()
         self.add(hbox)
-        hbox.show()        
+        hbox.show()
     
     def do_process(self, file, text, index):
-        return "__" + text
+        if text.startswith("__"):
+            return text.replace("__", "")
+        else:
+            return "__" + text
 
     def do_changed(self, widget):
         pass
 
     def do_get_actions(self, window, files):
         return [gtk.Action("TPR:SomeAction", "Some Action", None, gtk.STOCK_OPEN)]
+
+    def do_load(self, settings):
+        """
+        Loads settings saved in ~/.config/Thunar/renamerrc
+        """
+        print "loading settings"
+
+    def do_save(self, settings):
+        """
+        If do_save is overriden, you must rebuild the settings dictionary and then
+        return it.
+        """
+        settings["test"] = "blah"
+        
+        return settings
 
 class ThunarxRenamerPlugin(thunarx.RenamerProvider):
     def __init__(self):
